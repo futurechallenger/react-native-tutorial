850c8ffad12cbcd05953bc5cc976660224b71c6f
Uncle Charlie <lixiangdong@mobike.com>
2017-11-11 02:57:59 +0800

    pull to refresh & load more

--- js/message/MessageContainer.js
+++ js/message/MessageContainer.js
@@ -10,24 +10,75 @@ import {
 
 import MessageCell from './MessageCell';
 
-const {width, height} = Dimensions.get('window');
+const { width, height } = Dimensions.get('window');
 const SCREEN_WIDTH = width;
 
 export default class MessageContainer extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      error: false,
+      page: 1,
+      refreshing: false,
+      loading: false,
+    };
+  }
+
+  componentDidMount() {
+    this.requestData();
+  }
+
+  requestData = () => {
+    const url = 'https://www.apple.com';
+    fetch(url).then(res => res.json()).then(res => {
+      this.setState({
+        data: [...this.state.data, ...res.result],
+        error: res.error || null,
+        laoding: false,
+        refreshing: false,
+      });
+    }).catch(err => {
+      this.setState({ error: err, loading: false, refreshing: false});
+    });
+  };
+
+  handleRefresh = () => {
+    this.setState({
+      page: 1,
+      refreshing: true,
+      loading: false,
+    }, () => {
+      this.requestData();
+    });
+  };
+
+  handleLoadMore = () => {
+    this.setState({
+      page: this.state.page + 1,
+    }, () => {
+      this.requestData();
+    });
+  };
+
   //TODO: catch error
   componentDidCatch(error, info) {
     // this.setState
   }
-  
+
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch',backgroundColor: 'white' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'stretch', backgroundColor: 'white' }}>
         <Text>Message</Text>
         <FlatList
           data={[{ key: 'a' }, { key: 'b' }, { key: 'c' }, { key: 'd' }]}
           renderItem={({ item }) => (
-            <MessageCell />   
-          )} />
+            <MessageCell />
+          )}
+          refreshing={this.state.refreshing}
+          onRefresh={this.handleRefresh}
+          onEndReached={this.handleLoadMore}
+          onEndReachedThreshold={0} />
       </View>
     );
   }
