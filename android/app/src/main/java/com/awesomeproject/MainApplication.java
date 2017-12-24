03cd60374f1ed15785bd52a554afb04fc9d5cc19
Uncle Charlie <lixiangdong@mobike.com>
2017-12-24 20:29:11 +0800

    android load something

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -1,19 +1,37 @@
 package com.awesomeproject;
 
 import android.app.Application;
-
 import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-
+import java.io.File;
 import java.util.Arrays;
 import java.util.List;
+import javax.annotation.Nullable;
 
 public class MainApplication extends Application implements ReactApplication {
 
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+  private final ReactNativeHost mReactNativeHost = new CustomReactNativeHost(this);
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+
+  static class CustomReactNativeHost extends ReactNativeHost {
+
+    protected CustomReactNativeHost(Application application) {
+      super(application);
+    }
+
     @Override
     public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
@@ -30,16 +48,17 @@ public class MainApplication extends Application implements ReactApplication {
     protected String getJSMainModuleName() {
       return "index";
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
+    @Nullable
+    @Override
+    protected String getJSBundleFile() {
+      String jsBundleFile =
+          getApplication().getFilesDir().getAbsolutePath() + "/index.android.bundle";
+      File file = new File(jsBundleFile);
+      if (file == null || !file.exists()) {
+        return super.getJSBundleFile();
+      }
+      return jsBundleFile;
+    }
   }
 }
